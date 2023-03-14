1131dc82ed9ca151405f249a43cbddd2fe508774
Anthony <tony.raiti@gmail.com>
2023-03-14 13:43:34 -0400

    Adding deck model and inital endpoints

--- PyAPI/main.py
+++ PyAPI/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from pathlib import Path
@@ -9,6 +10,7 @@ import configparser
 from functools import wraps
 
 app = Flask(__name__)
+CORS(app)
 config = configparser.ConfigParser()
 config.read(Path(__file__).with_name('config.ini'))
 
@@ -24,6 +26,31 @@ class User(db.Model):
     hash = db.Column(db.String(255))
     salt = db.Column(db.String(255))
     admin = db.Column(db.Boolean)
+
+class Userdeck(db.Model):
+    userid = db.Column(db.Integer, db.ForeignKey('user.id'))
+    deckid = db.Column(db.Integer, db.ForeignKey('deck.id'))
+
+class Deck(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    link = db.Column(db.String(255))
+    lastused = db.Column(db.DateTime)
+    commander = db.Column(db.String(64))
+    partner = db.Column(db.String(64))
+    companion = db.Column(db.String(64))
+    power = db.Column(db.Integer)
+    identityid = db.Column(db.Integer, db.ForeignKey('coloridentity.id'))
+
+class Coloridentity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(16), uniqe=True)
+    blue = db.Column(db.Boolean)
+    white = db.Column(db.Boolean)
+    green = db.Column(db.Boolean)
+    red = db.Column(db.Boolean)
+    black = db.Column(db.Boolean)
+
 
 def token_required(f):
     @wraps(f)
@@ -45,9 +72,15 @@ def token_required(f):
 
     return decorated
 
+###############################################
+# User
+###############################################
 @app.route('/user', methods=['POST'])
 @token_required
 def create_user(current_user):
+    if not current_user.admin:
+        return jsonify({'message' : 'Lacking Permissions'})
+
     data = request.get_json()
     print(data['username'])
     user = User.query.filter_by(username=data['username']).first()
@@ -66,6 +99,9 @@ def create_user(current_user):
 @app.route('/user/<username>', methods=['PUT'])
 @token_required
 def update_user(current_user, username):
+    if not current_user.admin:
+        return jsonify({'message' : 'Lacking Permissions'})
+
     data = request.get_json()
 
     user = User.query.filter_by(username=username).first()
@@ -78,23 +114,60 @@ def update_user(current_user, username):
 
     return jsonify({'message' : 'Updated user'})
 
-@app.route('/login')
-def login():
-    auth = request.authorization
 
-    if not auth or not auth.username or not auth.password:
+###############################################
+# Deck
+###############################################
+@app.route('/deck', methods=['POST'])
+@token_required
+def create_deck(current_user):
+    data = request.get_json()
+
+    new_deck = Deck(name=data['name'], link=data['link'], commander=data['commander'], partner=data['partner'], companion=data['companion'], power=data['power'], identityid=data['identityid'])
+    print(new_deck.id)
+    new_userdeck = Userdeck(userid=current_user.id, deckid=new_deck.id)
+    db.session.add(new_deck)
+    db.session.add(new_userdeck)
+    db.session.commit()
+
+    return jsonify({'message' : 'New deck created'})
+
+@app.route('/user/<deckid>', methods=['PUT'])
+@token_required
+def update_user(current_user, deckid):
+    if not current_user.admin:
+        return jsonify({'message' : 'Lacking Permissions'})
+    
+    data = request.get_json()
+    deck = Deck.query.filter_by(id=deckid).first()
+    if not deck:
+        return jsonify({'message' : 'No deck found!'})
+    
+    if data['name']:
+        deck.name = data['name']
+
+    db.session.commit()
+    return jsonify({'message' : 'Updated user'})
+
+
+###############################################
+# Login
+###############################################
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    if not data or not data['username'] or not data['password']:
         return make_response('Could not verify', 401, {'WWW-authenticate' : 'Basic realm="Login Required"'})
     
-    user = User.query.filter_by(username=auth.username).first()
-
+    user = User.query.filter_by(username=data['username']).first()
     if not user:
         return jsonify('Could not verify', 401, {'WWW-authenticate' : 'Basic realm="Login Required"'})
 
-    if check_password_hash(user.hash, auth.password):
+    if check_password_hash(user.hash, data['password']):
         token = jwt.encode({'username': user.username, 'publicid':user.publicid, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, app.config['SECRET_KEY'])
-        return jsonify({'token': token})
+        return jsonify({'token': token, 'username': user.username}) #TODO: Define a model for the response data
 
-    return jsonify('Could not verify' + user.hash + auth.password, 401, {'WWW-authenticate' : 'Basic realm="Login Required"'})
+    return jsonify('Could not verify' + user.hash + data['password'], 401, {'WWW-authenticate' : 'Basic realm="Login Required"'})
 
 if __name__ == "__main__":
     app.run(debug=True)
