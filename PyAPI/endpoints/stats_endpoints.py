63ad39c46bb3a542dc81975c1dcd8faa06ad3c43
Anthony <tony.raiti@gmail.com>
2024-01-28 11:23:09 -0500

    adding basic user stats

--- PyAPI/endpoints/stats_endpoints.py
+++ PyAPI/endpoints/stats_endpoints.py
@@ -7,18 +7,28 @@ from helpers import scryfall_color_converter
 from models import User, Card, Deck, Decklist, Performance, Coloridentity
 from main import app, limiter, token_required, db
 
-@app.route('/stats/user/<id>', methods=['GET'])
+@app.route('/stats/user/<id>/simple', methods=['GET'])
 @token_required
 @limiter.limit('')
 def get_user_stats(current_user, id):
-    user = User.query.filter_by(publicid=id).first()
+    user = User.query.filter_by(id=id).first()
     if not user:
         return jsonify({'message' : 'No user found!'}), 204
     
     performances = Performance.query.filter_by(userid=user.id).all()
 
-    matches_played = performances.count()
-    #matches_won = 
+    matches_played = 0
+    matches_won = 0
+    average_placement = 0
 
-        
-    return jsonify({"matches_played": matches_played})
+    for p in performances:
+        if p.placement == None:
+            continue
+        if p.placement == 1:
+            matches_won += 1
+        matches_played += 1
+        average_placement += p.placement
+    if matches_played > 0:
+        average_placement = average_placement/matches_played
+    
+    return jsonify({"matchesplayed": matches_played, "matcheswon": matches_won, "averageplacement": average_placement})
--- PyAPI/main.py
+++ PyAPI/main.py
@@ -54,6 +54,7 @@ from endpoints.match_endpoints import *
 from endpoints.performance_endpoints import *
 from endpoints.color_endpoints import *
 from endpoints.card_endpoints import *
+from endpoints.stats_endpoints import *
 
 if __name__ == "__main__":
     app.run(debug=True)
